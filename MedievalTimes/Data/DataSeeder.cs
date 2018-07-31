f79f9a1e3f7ee8178b4e9423892a612b747bd6f8
Starquinius <lordtarquinius@hotmail.com>
2018-07-31 15:15:17 +0200

    DataSeed Table: Corrected Human Entry

--- MedievalTimes/Data/DataSeeder.cs
+++ MedievalTimes/Data/DataSeeder.cs
@@ -43,31 +43,35 @@ namespace MedievalTimes.Data
             }
 
             //Seed Races
-            if (_context.Races.Count() != 0)
+            if (_context.Races.Count() == 0)
             {
                 Race[] race = new Race[]{
-                    new Race{Name="Human"},
-                    new Race{Name="Elf"},
-                    new Race{Name="Dwarf"},
-                    new Race{Name="Gnome"},
-                    new Race{Name="Half-Elf"},
-                    new Race{Name="Halfling"}
+                    new Race{Name="Human",BaseAge=15, AgeModifier = new Dice(){ NrDice=1, DiceSide=4, Modifier=0}, BaseMaxAge=90, MaxAgeModifier = new Dice(){NrDice=2, DiceSide=20, Modifier=0}, BaseHeightF=59, BaseHeightM=60, HeightModifier = new Dice{NrDice=2, DiceSide=10, Modifier=0 }, BaseWeightF=100, BaseWeightM=140, WeightModifier=new Dice{ NrDice=6, DiceSide=10, Modifier=0}, RaceId = new Guid() },
+                    new Race{Name="Elf",BaseAge=100, AgeModifier = new Dice(){ NrDice=5, DiceSide=6, Modifier=0}, BaseMaxAge=350, MaxAgeModifier = new Dice(){NrDice=4, DiceSide=100, Modifier=0}, BaseHeightF=50, BaseHeightM=55, HeightModifier = new Dice{NrDice=1, DiceSide=10, Modifier=0 }, BaseWeightF=70, BaseWeightM=90, WeightModifier=new Dice{ NrDice=3, DiceSide=10, Modifier=0}, RaceId = new Guid() },
+                    new Race{Name="Dwarf",BaseAge=40, AgeModifier = new Dice(){ NrDice=5, DiceSide=6, Modifier=0}, BaseMaxAge=250, MaxAgeModifier = new Dice(){NrDice=2, DiceSide=100, Modifier=0}, BaseHeightF=41, BaseHeightM=43, HeightModifier = new Dice{NrDice=1, DiceSide=10, Modifier=0 }, BaseWeightF=105, BaseWeightM=130, WeightModifier=new Dice{ NrDice=4, DiceSide=10, Modifier=0}, RaceId = new Guid() },
+                    new Race{Name="Gnome",BaseAge=60, AgeModifier = new Dice(){ NrDice=3, DiceSide=12, Modifier=0}, BaseMaxAge=200, MaxAgeModifier = new Dice(){NrDice=3, DiceSide=100, Modifier=0}, BaseHeightF=36, BaseHeightM=38, HeightModifier = new Dice{NrDice=1, DiceSide=6, Modifier=0 }, BaseWeightF=68, BaseWeightM=72, WeightModifier=new Dice{ NrDice=5, DiceSide=4, Modifier=0}, RaceId = new Guid() },
+                    new Race{Name="Half-Elf",BaseAge=15, AgeModifier = new Dice(){ NrDice=1, DiceSide=6, Modifier=0}, BaseMaxAge=125, MaxAgeModifier = new Dice(){NrDice=3, DiceSide=20, Modifier=0}, BaseHeightF=58, BaseHeightM=60, HeightModifier = new Dice{NrDice=2, DiceSide=6, Modifier=0 }, BaseWeightF=85, BaseWeightM=110, WeightModifier=new Dice{ NrDice=3, DiceSide=12, Modifier=0}, RaceId = new Guid() },
+                    new Race{Name="Halfling",BaseAge=20, AgeModifier = new Dice(){ NrDice=3, DiceSide=4, Modifier=0}, BaseMaxAge=100, MaxAgeModifier = new Dice(){NrDice=1, DiceSide=100, Modifier=0}, BaseHeightF=30, BaseHeightM=32, HeightModifier = new Dice{NrDice=2, DiceSide=8, Modifier=0 }, BaseWeightF=48, BaseWeightM=52, WeightModifier=new Dice{ NrDice=5, DiceSide=4, Modifier=0}, RaceId = new Guid() }
                 };
                 await _context.AddRangeAsync(race);
                 var races = race.ToList();
 
                 //Seed Table (Race Attribute Requirement & Interval)
-                if (_context.RacialAttrReq.Count() != 0)
+                if (_context.RacialAttrReq.Count() == 0)
                 {
                     RacialAttributeAdjustment[] raceAttrAdj = new RacialAttributeAdjustment[] {
-                        new RacialAttributeAdjustment{Race=races[0]},
+                        new RacialAttributeAdjustment{Race=races[0], MinStr=3, MaxStr=18, MinDex=3, MaxDex=18, MinCon=3, MaxCon=18, MinInt=3, MaxInt=18, MinWis=3, MaxWis=18, MinCha=3, MaxCha=18},
                         new RacialAttributeAdjustment{Race=races[1], MinStr=3, MaxStr=18, MinDex=6, MaxDex=18, MinCon=7, MaxCon=18, MinInt=8, MaxInt=18, MinWis=3, MaxWis=18, MinCha=8, MaxCha=18, DexModifier=1, ConModifier=-1 },
                         new RacialAttributeAdjustment{Race=races[2], MinStr=8, MaxStr=18, MinDex=3, MaxDex=17, MinCon=11, MaxCon=18, MinInt=3, MaxInt=18, MinWis=3, MaxWis=18, MinCha=3, MaxCha=17, ConModifier=1, ChaModifier=-1 },
                         new RacialAttributeAdjustment{Race=races[3], MinStr=6, MaxStr=18, MinDex=3, MaxDex=18, MinCon=8, MaxCon=18, MinInt=6, MaxInt=18, MinWis=3, MaxWis=18, MinCha=3, MaxCha=18, IntModifier=1, WisModifier=-1 },
                         new RacialAttributeAdjustment{Race=races[4], MinStr=3, MaxStr=18, MinDex=6, MaxDex=18, MinCon=6, MaxCon=18, MinInt=4, MaxInt=18, MinWis=3, MaxWis=18, MinCha=3, MaxCha=18 },
                         new RacialAttributeAdjustment{Race=races[5], MinStr=7, MaxStr=18, MinDex=7, MaxDex=18, MinCon=10, MaxCon=18, MinInt=6, MaxInt=18, MinWis=3, MaxWis=17, MinCha=3, MaxCha=18, DexModifier=1, StrModifier=-1 }
                     };
+
+                    await _context.AddRangeAsync(raceAttrAdj);
                 }
+
+                await _context.SaveChangesAsync();
             }
         }
     }
