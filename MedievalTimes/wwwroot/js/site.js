982f5a04580b6808f9beb2a04507a76e0a72efac
Starquinius <lordtarquinius@hotmail.com>
2018-07-31 16:29:00 +0200

    JScript : Create Character : Attributes randomly filled, can reroll x-times

--- MedievalTimes/wwwroot/js/site.js
+++ MedievalTimes/wwwroot/js/site.js
@@ -1,4 +1,64 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+window.onload = Initieer;
 
-// Write your JavaScript code.
+//**************************************************************************************************************************************************************** VARIABLES ***
+
+// max reroll possibilities when creating character
+var reroll = 60;
+
+//Init Attribute variables
+var strength = rollDice(3, 6, 0);
+var dexterity = rollDice(3, 6, 0);
+var constitution = rollDice(3, 6, 0);
+var intelligence = rollDice(3, 6, 0);
+var wisdom = rollDice(3, 6, 0);
+var charisma = rollDice(3, 6, 0);
+
+//****************************************************************************************************************************************************************** METHODS ***
+
+//Init Function
+function Initieer() {
+    $("#rollAttributes").click(ReRoll);
+    SetDefaultAttributes();
+}
+
+// This function will set the values for the attributes, when creating a character
+function SetDefaultAttributes() {
+    $("#strAttribute").val(strength);
+    $("#dexAttribute").val(dexterity);
+    $("#conAttribute").val(constitution);
+    $("#intAttribute").val(intelligence);
+    $("#wisAttribute").val(wisdom);
+    $("#chaAttribute").val(charisma);
+}
+
+// This function rolls dices and returns the total value. 
+// Example 3d6 + 2, 3 dices, 6 sided, +2 modifier at the end
+function rollDice(nrDice, DiceSide, Modifier) {
+    var totalPoints = 0;
+    for (var teller = 0; teller < nrDice; teller++)
+        totalPoints += Math.floor(Math.random() * DiceSide) + 1;
+    totalPoints += Modifier;
+    return totalPoints;
+}
+
+//This function rerolls the attributes for character creation, but not more than default times.
+function ReRoll() {
+    if (reroll <= 0) {
+        alert("Their are no new rolls possible.");
+    }
+    else {
+        strength = rollDice(3, 6, 0);
+        dexterity = rollDice(3, 6, 0);
+        constitution = rollDice(3, 6, 0);
+        intelligence = rollDice(3, 6, 0);
+        wisdom = rollDice(3, 6, 0);
+        charisma = rollDice(3, 6, 0);
+
+        SetDefaultAttributes();
+
+        reroll--;
+
+        $("#rollAttributes").text("ReRoll Dices (" + reroll + ")");
+    }
+}
